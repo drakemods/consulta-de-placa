e1fd3fc5815af734bc467cdfc8e0f3fc12a22f4e
Drake <83844803+drakemods@users.noreply.github.com>
2022-02-06 21:30:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const placa = prompt("Digite a placa do veiculo: ")
     sinesp.search(`${placa}`).then(async (dados) => {
         console.log(`\nPlaca: ${dados.placa}\n\nSituação: ${dados.situacao}\nModelo: ${dados.modelo}\nMarca: ${dados.marca}\nCor: ${dados.cor}\nAno: ${dados.ano}\nAno do modelo: ${dados.anoModelo}\nEstado: ${dados.uf}\nMunicipio: ${dados.municipio}\nChassi: ${dados.chassi}.\n`)
     }).catch(async (error) => {
-        console.log('Nenhuma placa encontrada, verifique a placa!')
+        console.log('Placa não encontrada, verifique a placa!')
     })
 
     // Formato/placa de exemplo: BHS1192
